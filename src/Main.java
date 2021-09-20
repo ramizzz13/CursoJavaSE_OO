797d943013163e1ae6f2ada69adff9ebb847510b
Ramiz Perez <ramizzz13@gmail.com>
2021-09-20 12:05:00 -0500

    Método constructor

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Ramiz Perez");
         myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName();
 
