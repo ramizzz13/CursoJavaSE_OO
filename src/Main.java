2a6e665d1bb996861604e6c36edb0418722579f1
Ramiz Perez <ramizzz13@gmail.com>
2021-09-20 15:44:52 -0500

    Getters y Setters

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,10 @@ public class Main {
         System.out.println(myDoctor.speciality);
 
         Patient patient = new Patient("Alejandra", "alejandra@gmail.com");
-
+        patient.setWeight(54.5);
+        System.out.println(patient.getWeight());
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());
 
     }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -14,4 +14,80 @@ public class Patient {
         this.email = email;
     }
 
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+
+    public String getWeight(){
+        return this.weight + " Kgs.";
+    }
+
+    public String  setHeight() {
+        return this.height + " Mts.";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+
+        if (phoneNumber.length() > 8){
+            System.out.println("El numero de teléfono debe ser de máximo 8 digitos.");
+        } else if (phoneNumber.length() == 8){
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
 }
